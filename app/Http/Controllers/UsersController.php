e07be38668436bc7a53238bb22bc02d8f0ef4deb
ssl <kjcy8@qq.com>
2018-04-14 09:22:36 +0000

    finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,51 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
+use App\Models\User;
 
 class UsersController extends Controller
 {
+    /*
+     * 创建用户
+     *
+     */
     public function create()
     {
         return view('users.create');
     }
+
+    /*
+     * 显示用户信息
+     *
+     */
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    /*
+     * 保存用户信息
+     *
+     */
+    public function store(Request $request)
+    {
+        // 验证
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            //'email' => 'required|email|unqiue:users|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        // 入库
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
+    }
+
 }
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,7 +1,45 @@
 @extends('layouts.default')
 
-@section('title', 'signup');
+@section('title', 'signup')
 
 @section('content')
-    <h1>sign up</h1>
+    <div class="col-md-offset-2 col-md-8">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h5>sign up</h5>
+            </div>
+
+            <div class="panel-body">
+                <form method="POST" action="{{ route('users.store') }}">
+                    @include('shared._errors')
+
+                    {{ csrf_field() }}
+
+                    <div class="form-group">
+                        <label for="name">name：</label>
+                        <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">email：</label>
+                        <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">password：</label>
+                        <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password_confirmation">confirm password：</label>
+                        <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">sign up</button>
+
+                </form>
+            </div>
+        </div>
+
+    </div>
 @stop
